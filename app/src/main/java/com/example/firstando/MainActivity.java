a1900a7ae6e0131ac2815a9d4e84a9d016abfda2
CHAKFI Ahmed <ahmedchakfi1999@gmail.com>
2022-03-17 22:37:26 +0100

    Setup onClick for Verify button

--- app/src/main/java/com/example/firstando/MainActivity.java
+++ app/src/main/java/com/example/firstando/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.firstando;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,19 +20,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Declaring variables
-        EditText nm = findViewById(R.id.name);
-        EditText bt = findViewById(R.id.birth);
+
         Button ve = findViewById(R.id.verify);
         Button ca = findViewById(R.id.cancel);
-        TextView dispName = findViewById(R.id.nameText);
-        TextView age = findViewById(R.id.resultText);
+
 
         ve.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onClick(View view) {
 
-                
 
+                EditText nm = findViewById(R.id.name);
+                EditText bt = findViewById(R.id.birth);
+                TextView dispName = findViewById(R.id.nameText);
+                TextView res = findViewById(R.id.resultText);
+
+                String n = nm.getText().toString();
+                int b = Integer.parseInt(bt.getText().toString());
+                int age = LocalDate.now().getYear() + 1900 - b;
+
+                dispName.setText("Hi, "+nm);
+                res.setText(age);
             }
         });
 
